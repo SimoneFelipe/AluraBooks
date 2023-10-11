cf0ef7550eaee55b737108262fbfc9f3bb9f5373
Simone Correa Felipe <77705412+SimoneFelipe@users.noreply.github.com>
2023-10-11 15:57:14 -0300

    Add files via upload

---
+++ app/main.js
@@ -0,0 +1,10 @@
+let livros = []
+const endpointDaAPI = 'https://guilhermeonrails.github.io/casadocodigo/livros.json'
+getBuscarLivrosDaAPI()
+
+async function getBuscarLivrosDaAPI() {
+    const res = await fetch(endpointDaAPI)
+    livros = await res.json()
+    livros = aplicarDesconto(livros)
+    exibirOsLivrosNaTela(livrosComDesconto)
+}
---
+++ app/metodoForEach.js
@@ -0,0 +1,33 @@
+const elementoParaInserirLivros = document.getElementById('livros')
+const elementoComValorTotalDeLivrosDisponiveis = document.getElementById('valor_total_livros_disponiveis')
+
+function exibirOsLivrosNaTela(listaDeLivros) {
+    elementoComValorTotalDeLivrosDisponiveis.innerHTML = ''
+    elementoParaInserirLivros.innerHTML = ''
+    listaDeLivros.forEach(livro => {
+        // let disponibilidade = verificarDisponibilidadeDoLivro(livro)
+        let disponibilidade = livro.quantidade > 0 ? 'livro__imagens' : 'livro__imagens indisponivel' 
+        elementoParaInserirLivros.innerHTML += `
+        <div class="livro">
+        <img class="${disponibilidade}" src="${livro.imagem}"
+          alt="${livro.alt}" />
+        <h2 class="livro__titulo">
+          ${livro.titulo}
+        </h2>
+        <p class="livro__descricao">${livro.autor}</p>
+        <p class="livro__preco" id="preco">R$${livro.preco.toFixed(2)}</p>
+        <div class="tags">
+          <span class="tag">${livro.categoria}</span>
+        </div>
+      </div>
+        `
+    })
+}
+
+// function verificarDisponibilidadeDoLivro(livro) {
+//   if (livro.quantidade > 0 ){
+//     return 'livro__imagens'
+//   } else {
+//     return 'livros__imagens indisponivel'
+//   }
+// }
